aae3e6ba8dd50c8987162f51617ed6336b9371c5
Murtadha9 <murtadha.hussen@gmail.com>
2024-05-10 14:32:02 +0300

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 
 import React from 'react'
+import {BrowserRouter ,Route ,Routes} from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import SignIn from './Pages/SignIn/SignIn'
+import SignUp from './Pages/SignUp/SignUp'
+import Profile from './Pages/Profile/Profile'
+import NotFound from './Pages/NotFound/NotFound'
+import Header from './Components/Header/Header'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
