8279b340b6d886e371584d8b312af127ebc732a9
Murtadha9 <murtadha.hussen@gmail.com>
2024-05-11 16:09:55 +0300

    middleWare

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+import userRoute from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
 
 
 const app= express();
+dotenv.config()
+
 
 
 app.listen(3000,()=>{
@@ -9,10 +16,22 @@ app.listen(3000,()=>{
 });
 
 
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('connected to mongodb');
+}).catch((error)=>{console.log(error)})
 
+//Endpoints
+app.use('/api/user', userRoute)
+app.use('/api/auth', authRoute)
 
-
-
-
-
+//MiddleWare
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
 
